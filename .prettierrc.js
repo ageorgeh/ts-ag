f4266565e4995fde1a2ce89d5a7ebc7ac05895d1
Alexander Hornung <aghornung@gmail.com>
2026-01-24 08:35:21 +1100

    Upgrades

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,16 +1,17 @@
-
 /**
  * @see https://prettier.io/docs/configuration
  * @type {import("prettier").Config}
  */
 const config = {
-  useTabs: false, 
+  useTabs: false,
   singleQuote: true,
   trailingComma: 'none',
   printWidth: 120,
+  objectWrap: 'collapse',
   semi: true,
   proseWrap: 'always',
+  svelteSortOrder: 'scripts-options-markup-styles',
+  plugins: ['prettier-plugin-packagejson']
+};
 
-}
- 
-export default config;    
+export default config;
