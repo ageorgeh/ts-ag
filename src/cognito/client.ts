e05ee7bf229941dcd125a094519aa3264cc9d9d2
Alexander Hornung <aghornung@gmail.com>
2026-03-11 20:13:39 +1100

    fix: default to AWS_REGION

--- src/cognito/client.ts
+++ src/cognito/client.ts
@@ -1,11 +1,11 @@
 import { CognitoIdentityProviderClient } from '@aws-sdk/client-cognito-identity-provider';
 
 /**
- * Convenience function if process.env.REGION is set
+ * Convenience function if process.env.AWS_REGION is set
  */
 export function getCognitoClient() {
   return new CognitoIdentityProviderClient({
-    region: process.env.REGION,
-    endpoint: `https://cognito-idp.${process.env.REGION}.amazonaws.com`
+    // region: process.env.AWS_REGION
+    // endpoint: `https://cognito-idp.${process.env.REGION}.amazonaws.com`
   });
 }
--- src/s3/client.ts
+++ src/s3/client.ts
@@ -4,5 +4,8 @@ import { S3Client } from '@aws-sdk/client-s3';
  * Convenience function for S3Client when process.env.REGION is set
  */
 export function getS3() {
-  return new S3Client({ endpoint: `https://s3.${process.env.REGION}.amazonaws.com`, region: process.env.REGION });
+  return new S3Client({
+    // endpoint: `https://s3.${process.env.REGION}.amazonaws.com`,
+    // region: process.env.REGION
+  });
 }
