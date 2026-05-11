cc09869461d0010cf0c4711c871a35f9c13d50be
Alexander Hornung <aghornung@gmail.com>
2026-05-11 22:29:28 +1000

    fix: allow s3 client overrides

--- src/s3/client.ts
+++ src/s3/client.ts
@@ -1,10 +1,11 @@
-import { S3Client } from '@aws-sdk/client-s3';
+import { S3Client, type S3ClientConfig } from '@aws-sdk/client-s3';
 
 /**
  * Convenience function for S3Client when process.env.REGION is set
  */
-export function getS3() {
+export function getS3(args?: S3ClientConfig) {
   return new S3Client({
+    ...args
     // endpoint: `https://s3.${process.env.REGION}.amazonaws.com`,
     // region: process.env.REGION
   });
--- src/s3/object.ts
+++ src/s3/object.ts
@@ -1,24 +1,49 @@
-import { HeadObjectCommand, GetObjectCommand } from '@aws-sdk/client-s3';
+import {
+  HeadObjectCommand,
+  GetObjectCommand,
+  type GetObjectCommandInput,
+  type S3ClientConfig
+} from '@aws-sdk/client-s3';
 import { ResultAsync } from 'neverthrow';
 
 import { getS3 } from './client.js';
-import { error_s3, is_s3_notFound, type type_error_s3 } from './errors.js';
+import { error_s3, is_s3_notFound } from './errors.js';
 
-const getObjectResult = /* @__PURE__ */ ResultAsync.fromThrowable(
-  async (bucketName: string, key: string) => {
-    const s3 = getS3();
-    const cmd = new GetObjectCommand({ Bucket: bucketName, Key: key });
-    const res = await s3.send(cmd);
-    const stream = res.Body as any;
-    return new Promise<Buffer>((resolve, reject) => {
-      const chunks: Buffer[] = [];
-      stream.on('data', (chunk: Buffer) => chunks.push(chunk));
-      stream.on('end', () => resolve(Buffer.concat(chunks)));
-      stream.on('error', reject);
-    });
+/**
+ * Retrieves an object from an S3 bucket.
+ *
+ * @param {string} bucketName - The name of the S3 bucket.
+ * @param {string} key - The key of the object to retrieve.
+ */
+export const getObject = /* @__PURE__ */ ResultAsync.fromThrowable(
+  async (input: GetObjectCommandInput, args?: S3ClientConfig): Promise<Buffer> => {
+    const s3 = getS3(args);
+    const res = await s3.send(new GetObjectCommand(input));
+    if (!res.Body) return Buffer.alloc(0);
+    const bytes = await res.Body.transformToByteArray();
+    return Buffer.from(bytes);
   },
   (e) => {
-    console.error(`getObjectt: Error getting object from S3: ${e}`);
+    console.error('getObject: Error getting object from S3:', e);
+    return error_s3(e);
+  }
+);
+
+/**
+ * Convenience function to get an object from S3 and return it as a string.
+ *
+ * @param {string} bucketName - The name of the S3 bucket.
+ * @param {string} key - The key of the object to retrieve.
+ */
+export const getObjectString = /* @__PURE__ */ ResultAsync.fromThrowable(
+  async (input: GetObjectCommandInput, args?: S3ClientConfig): Promise<string> => {
+    const s3 = getS3(args);
+    const res = await s3.send(new GetObjectCommand(input));
+    if (!res.Body) return '';
+    return await res.Body.transformToString('utf-8');
+  },
+  (e) => {
+    console.error('getObjectString: Error getting object from S3:', e);
     return error_s3(e);
   }
 );
@@ -41,24 +66,6 @@ const objectExistsResult = /* @__PURE__ */ ResultAsync.fromThrowable(
     return error_s3(e);
   }
 );
-
-/**
- * Retrieves an object from an S3 bucket.
- *
- * @param {string} bucketName - The name of the S3 bucket.
- * @param {string} key - The key of the object to retrieve.
- * @returns {Promise<Buffer>} A promise that resolves to the object data as a Buffer.
- */
-export function getObject(bucketName: string, key: string) {
-  return getObjectResult(bucketName, key);
-}
-
-/**
- * Convenience function to get an object from S3 and return it as a string.
- */
-export function getObjectString(bucketName: string, key: string): ResultAsync<string, type_error_s3> {
-  return getObject(bucketName, key).map((buffer) => buffer.toString('utf-8'));
-}
 
 /**
  * Checks if an object exists in an s3 bucket by retrieving the HEAD data
